5c3b8fb2c473c4509bd2291241ae47f8562aac59
Chetra Mo <chetrahusky@gmail.com>
2019-08-19 21:43:30 -0400

    Finished some features for ?info. Smashed some bugs.

--- driver.py
+++ driver.py
@@ -6,22 +6,19 @@ from dtoken import TOKEN
 from discord.ext import commands
 import info_methods
 
+MAX_LEVEL_TWO = "Description2"
+MAX_LEVEL_THREE = "Description3"
 bot = commands.Bot(command_prefix="?")
 
-# start-up (terminal)
 @bot.event
 async def on_ready():
     print("I might be in a bathing suit, but I can, and will, continue to maintain order.")
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Dragalia Lost"))
 
-# ?notte sends a silly message
 @bot.command()
 async def notte(ctx):
     await ctx.send("Sweet sassy molassy!")
 
-# ?info: sends an embedded Wiki entry about that character
-# input: arg - string sent in by user
-# output: sends embedded description of entity to discord channel the command was entered in
 @bot.command()
 async def info(ctx, *, arg: str):
     """
@@ -31,20 +28,36 @@ async def info(ctx, *, arg: str):
     :return: None
     """
     entity = arg
-    adventurerDict = {}
-    print(entity)
+    entityDict = {}
+
     try:
-        adventurerDict = info_methods.retrieve_dict(entity)
+        entityDict = info_methods.make_dict(entity)
     except AttributeError:
-        print("Inside except")
         await ctx.send(f"{entity} does not exist. Are you sure you spelled their name right?")
     else:
-        embed = discord.Embed(title="Adventurer", description=adventurerDict["FullName"], color=0x3D85C6)
-        embed.set_thumbnail(url="https://gamepedia.cursecdn.com/dragalialost_gamepedia_en/c/c5/110063_01_r04.png?version=012dea440c42c82f0f8bc86b9e5f5453")
-        embed.add_field(name="Skill 1", value=adventurerDict["Skill1Name"], inline=False)
-        embed.add_field(name="Skill 2", value=adventurerDict["Skill2Name"], inline=False)
-        embed.set_author(name="Dragalia Lost Wiki", url="https://dragalialost.gamepedia.com")
-        await ctx.send(embed=embed)
-
+        if "adventurer" or "dragon" in entityDict:
+            if entityDict["entityType"] == "adventurer":
+                embed = discord.Embed(title="Adventurer", description=entityDict["FullName"], color=0x3D85C6)
+                imageLink = info_methods.get_image(entityDict["entityType"], entityDict["Rarity"],
+                                                   entityDict["Id"], entityDict["VariationId"])
+                embed.set_thumbnail(url=imageLink)
+                skillDescriptionOne = info_methods.pretty_print(entityDict["Skill1Name"],
+                                                  info_methods.get_skill(entityDict["Skill1Name"], MAX_LEVEL_THREE))
+                skillDescriptionTwo = info_methods.pretty_print(entityDict["Skill2Name"],
+                                                  info_methods.get_skill(entityDict["Skill2Name"], MAX_LEVEL_TWO))
+                embed.add_field(name="Skill 1", value=skillDescriptionOne, inline=False)
+                embed.add_field(name="Skill 2", value=skillDescriptionTwo, inline=False)
+                embed.set_author(name="Dragalia Lost Wiki", url="https://dragalialost.gamepedia.com")
+                await ctx.send(embed=embed)
+            elif entityDict["entityType"] == "dragon":
+                embed = discord.Embed(title="Dragon", description=entityDict["FullName"], color=0x3D85C6)
+                imageLink = info_methods.get_image(entityDict["entityType"], entityDict["Rarity"],
+                                                   entityDict["BaseId"], entityDict["VariationId"])
+                embed.set_thumbnail(url=imageLink)
+                skillDescriptionOne = info_methods.pretty_print(entityDict["SkillName"],
+                                                  info_methods.get_skill(entityDict["SkillName"], MAX_LEVEL_TWO))
+                embed.add_field(name="Skill", value=skillDescriptionOne, inline=False)
+                embed.set_author(name="Dragalia Lost Wiki", url="https://dragalialost.gamepedia.com")
+                await ctx.send(embed=embed)
 
 bot.run(TOKEN)
--- info_methods.py
+++ info_methods.py
@@ -2,24 +2,92 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def retrieve_dict(entity):
+def make_dict(entityName):
     """
-    Retrieves table content from entity page and puts it in a dict
-    :param entity: String entered in by user containing entity name
-    :return: adventurerDict, dict containing entity info
+    Retrieves table content from entity page and puts it in a dict.
+    :param entityName: string entered in by user containing entity name
+    :return: dict containing entity info
     """
-    # retrieve data from website
-    response = requests.get(f"https://dragalialost.gamepedia.com/index.php?title={entity}&action=pagevalues")
+    response = requests.get(f"https://dragalialost.gamepedia.com/index.php?title={entityName.title()}&action=pagevalues")
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
+
     rawTable = soup.find("table", {"class": "wikitable mw-page-info"})
-
-    adventurerDict = {}
+    entityDict = {}
     # for each tr tag...
-    for trTag in rawTable.findAll("tr"):
+    for trTag in rawTable.find_all("tr"):
         # find all td tags (two exactly)
-        tdTags = trTag.findAll("td")
+        tdTags = trTag.find_all("td")
         # first column = key; second = value
-        adventurerDict[tdTags[0].string] = tdTags[1].string
+        entityDict[tdTags[0].string] = tdTags[1].string
 
-    return adventurerDict
+    spanTag = soup.find("span", {"class": "mw-headline"})
+    if "adventurer" in spanTag.text.lower():
+        entityDict["entityType"] = "adventurer"
+    elif "dragon" in spanTag.text.lower():
+        entityDict["entityType"] = "dragon"
+
+    return entityDict
+
+
+def get_image(entityType, rarity, entityID, variation):
+    """
+    Retrieves thumbnail of entity
+    :param entityType: string representing if the entity is an adventurer or dragon
+    :param rarity: integer representing the rarity of the entity
+    :param entityID: integer representing the ID of the entity
+    :param variation: integer representing if the entity requested is an original or themed variation
+    :return: url of the entity's thumbnail
+    """
+    if entityType == "adventurer":
+        response = requests.get(f"https://dragalialost.gamepedia.com/File:{entityID}_0{variation}_r0{rarity}.png")
+        html = response.text
+        soup = BeautifulSoup(html, "html.parser")
+        div = soup.find("div", {"class": "fullImageLink"})
+        link = div.find("a")["href"]
+        return link
+
+    elif entityType == "dragon":
+        response = requests.get(f"https://dragalialost.gamepedia.com/File:{entityID}_0{variation}.png")
+        html = response.text
+        soup = BeautifulSoup(html, "html.parser")
+        div = soup.find("div", {"class": "fullImageLink"})
+        link = div.find("a")["href"]
+        return link
+
+
+def get_skill(skillName, maxSkillLevel):
+    """
+    Retrieves skill description.
+    :param skillName: string representing skill name
+    :param maxSkillLevel: string representing highest level the skill can become
+    :return: string containing the concatenated description
+    """
+    skillDescription = ""
+    urlPart = skillName.replace(" ", "_")
+    response = requests.get(f"https://dragalialost.gamepedia.com/index.php?title={urlPart}&action=pagevalues")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    rawTable = soup.find_all("tr")
+    for trTag in rawTable:
+        tdTags = trTag.find_all("td")
+        if tdTags[0].string == "Description3" and tdTags[0].string == maxSkillLevel:
+            for i in tdTags[1]:
+                if i.string is not None:
+                    skillDescription += i.string
+        elif tdTags[0].string == "Description2" and tdTags[0].string == maxSkillLevel:
+            for i in tdTags[1]:
+                if i.string is not None:
+                    skillDescription += i.string
+    return skillDescription
+
+
+def pretty_print(skillName, description):
+    """
+    Formats the skill and its description so they can be embedded into Discord
+    :param skillName: string representing skill name
+    :param description: string representing lengthy skill description
+    :return: formatted string
+    """
+    return "**" + skillName + "** " + description
