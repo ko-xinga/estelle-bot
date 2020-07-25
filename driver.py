c7b72746c755e3747ee4db76fa6127c27f9854f7
ko-xinga <chetrahusky@gmail.com>
2020-07-24 22:05:57 -0400

    Removed logging.

--- driver.py
+++ driver.py
@@ -1,5 +1,4 @@
 import discord
-import logging
 from dtoken import TOKEN
 from discord.ext import commands
 import info_methods
@@ -222,12 +221,5 @@ async def notte(ctx):
     """
     await ctx.send("Sweet sassy molassy!")
 
-logger = logging.getLogger("discord")
-# set the threshold for the logger to level
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-# e.g. 2020-04-20 21:14 : 30,883 : DEBUG:discord.gateway
-handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
-logger.addHandler(handler)
 
 bot.run(TOKEN)
